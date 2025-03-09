82cd69b105819868fc08765f8f8fac799f86cc4d
Mathew Lopez <mathewlopez10@gmail.com>
2025-03-08 19:32:22 -0500

    Added PORT

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    host: '0.0.0.0'
   },
 });
