b6793be436af553cbbad489304bdab297550588c
Mathew Lopez <mathewlopez10@gmail.com>
2025-03-08 19:22:02 -0500

    Modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    host: '0.0.0.0'
-  },
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
